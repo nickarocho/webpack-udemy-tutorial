cf81c318cf628f8a7b94f1a908254716af309f15
Nick Arocho <nick@boldlyfine.com>
2019-05-16 17:20:36 -0700

    Add public path to config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ module.exports = {
     entry: './src/index.js', //Webpack entry point, usually imports all other dependencies
     output: { //specify the name of the file which will be generated as result of WP build
         filename: 'bundle.js', //what's the file called?
-        path: path.resolve(__dirname, './dist') //where will the file live? (auto-created by WP)
+        path: path.resolve(__dirname, './dist'), //where will the file live? (auto-created by WP)
         //WP needs 'path' as a helper to avoid this error: "configuration.output.path: The provided value "./dist" is not an absolute path!-> The output directory as **absolute path** (required)."
+        publicPath: 'dist/' //this tells the browser where to start the path for things like img src
     },
     mode: 'none', //mandatory option
     module: { //teaching WP how to import different file types (knows JS "by heart", but not other types)
