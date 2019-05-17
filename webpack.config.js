9876811d3b4dcdb1a13c4f8d1f8d4a8eec0dc1bd
Nick Arocho <nick@boldlyfine.com>
2019-05-16 17:00:26 -0700

    Complete basic Webpack integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path'); //doesn't support ES6 modules yet
+
 module.exports = {
     entry: './src/index.js', //Webpack entry point, usually imports all other dependencies
     output: { //specify the name of the file which will be generated as result of WP build
         filename: 'bundle.js', //what's the file called?
-        path: './dist' //where will the file live? (auto-created by WP)
+        path: path.resolve(__dirname, './dist') //where will the file live? (auto-created by WP)
+        //WP needs 'path' as a helper to avoid this error: "configuration.output.path: The provided value "./dist" is not an absolute path!-> The output directory as **absolute path** (required)."
     },
     mode: 'none' //mandatory option
 }
